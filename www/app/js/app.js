e0f24198b92e019ae015ee227d5b6f89ba6ed3b0
Lepikov Stanislav <lepikov.sy@vuaro.ru>
2014-12-09 15:42:44 +0300

    collect index page from modules

--- www/app/js/app.js
+++ www/app/js/app.js
@@ -3,29 +3,28 @@
 // Declare app level module which depends on views, and components
 var myApp = angular
 			.module('myApp', [
-			  'ngRoute',
-			  'ui.router'
+			  'ngRoute'
 			])
-			.config(['$routeProvider', '$stateProvider', function($routeProvider, $stateProvider) {
-				$routeProvider.
-					when('/', {
-						templateUrl: 'app/templates/main.htm',
+			.config(['$routeProvider', function($routeProvider) {
+				$routeProvider
+					.when('/', {
+						templateUrl: 'app/templates/pages/main.htm',
 						controller: 'MainCtrl'
-					});
-					
-				$stateProvider
-					.state('header', {
-						url: "/",
-						views: {
-							"header": { template: "app/templates/header.htm" }
-						}
+					})
+					.when('/askquestion', {
+						templateUrl: 'app/templates/pages/askquestion.htm',
+						controller: 'MainCtrl'
 					});
 			}]);
 
 /* Controllers */
-
 myApp.controller('MainCtrl',
-  function($scope) {
-	console.log('aeae')
-  }
+	function($scope) {
+		$scope.templates = {
+			header: 'app/templates/modules/header.htm',
+			carChoose: 'app/templates/modules/carChoose.htm',
+			comments: 'app/templates/modules/comments.htm',
+			footer: 'app/templates/modules/footer.htm'
+		}
+	}
 );
